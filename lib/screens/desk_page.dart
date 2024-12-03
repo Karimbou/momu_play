72f693ec98f06d15c08579b1022028f2700cd667
Karim Bouchouchi <karim@netzwiese.com>
2024-12-03 09:43:38 +0100

    Added Screen Desk Page

---
+++ lib/screens/desk_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class DeskPage extends StatefulWidget {
+  const DeskPage({super.key});
+
+  @override
+  State<DeskPage> createState() => _DeskPageState();
+}
+
+class _DeskPageState extends State<DeskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
